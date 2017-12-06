7ebaf8bbe31341a105a73f1e4f9b0497a83181c4
jesusQuintana-30-2017 <jesus.quintana30@gmail.com>
2017-12-06 16:13:42 -0600

    cambiso fantama

--- AgenteApp/AgenteApp/Clases/Constantes.cs
+++ AgenteApp/AgenteApp/Clases/Constantes.cs
@@ -7,10 +7,10 @@ namespace AgenteApp.Clases
     class Constantes
     {
         //public static string DIRECCION_BASE= "http://alanbazan.com.mx";
-        //public static string DIRECCION_BASE = "http://127.0.0.1:7000";
-        public static string DIRECCION_BASE = "http://10.25.1.250:81";
-        //public static string DIRECCION_ELAXTIC = "192.168.1.106";
-        public static string DIRECCION_ELAXTIC = "10.25.1.231";
+        public static string DIRECCION_BASE = "http://127.0.0.1:7001";
+        //public static string DIRECCION_BASE = "http://10.25.1.250:81";
+        public static string DIRECCION_ELAXTIC = "192.168.1.106";
+        //public static string DIRECCION_ELAXTIC = "10.25.1.231";
         public static string PASS_CALL = "bastiaan";
 
         public static int VERSION = 1;
