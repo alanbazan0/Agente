442c31b875bbdacda91ee244665a5302d5852816
Juan Salvador Villalobos Grijalva <el_salvador123@hotmail.com>
2018-02-06 10:02:43 -0600

    inicio de sesión con reconocimiento facial

---
+++ AgenteApp/AgenteApp/Clases/Utilerias.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace AgenteApp.Clases
+{
+    public static class Utilerias
+    {
+        public static async Task<byte[]> GetBytesAsync(StorageFile file)
+        {
+            byte[] fileBytes = null;
+            if (file == null) return null;
+            using (var stream = await file.OpenReadAsync())
+            {
+                fileBytes = new byte[stream.Size];
+                using (var reader = new DataReader(stream))
+                {
+                    await reader.LoadAsync((uint)stream.Size);
+                    reader.ReadBytes(fileBytes);
+                }
+            }
+            return fileBytes;
+        }
+
+        public async static Task<BitmapImage> BytesFromImage(Byte[] bytes)
+        {
+            BitmapImage image = new BitmapImage();
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+            {
+                await stream.WriteAsync(bytes.AsBuffer());
+                stream.Seek(0);
+                await image.SetSourceAsync(stream);
+            }
+            return image;
+        }
+
+        public static byte[] AsByteArray(this WriteableBitmap bitmap)
+        {
+            using (Stream stream = bitmap.PixelBuffer.AsStream())
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static string asByteToString(byte[] originalArray)
+        {
+            return Convert.ToBase64String(originalArray);
+        }
+
+        public static byte[] Base64ToByte(string temp_inBase64)
+        {
+            return Convert.FromBase64String(temp_inBase64);
+        }
+
+    }
+}
--- AgenteApp/AgenteApp/Presentadores/InicioSesionPresentador.cs
+++ AgenteApp/AgenteApp/Presentadores/InicioSesionPresentador.cs
@@ -20,7 +20,7 @@ namespace AgenteApp.Presenters
         {
             UsuarioRepositorio accesoDatos = new UsuarioRepositorio();
             Resultado<Usuario> resultado = await accesoDatos.Consultar(vista.NombreUsuario, vista.Contrasena);
-            if(resultado.mensajeError==string.Empty)
+            if (resultado.mensajeError == string.Empty)
             {
                 usuario = resultado.valor;
                 if (usuario != null)
@@ -33,14 +33,47 @@ namespace AgenteApp.Presenters
             }
             else
                 vista.MostrarMensaje(resultado.mensajeError);
+        }
 
-
+        public async void DatosSesion()
+        {
+            UsuarioRepositorio accesoDatos = new UsuarioRepositorio();
+            Resultado<Usuario> resultado = await accesoDatos.ConsultarDatosSesion(vista.NombreUsuario);
+            if (resultado.mensajeError == string.Empty)
+            {
+                usuario = resultado.valor;
+                if (usuario != null)
+                {
+                    vista.usuario = usuario;
+                }
+                else
+                    vista.MostrarMensaje("El usuario no existe, favor de verificar.");
+            }
+            else
+                vista.MostrarMensaje(resultado.mensajeError);
+        }
+        public async void SubirFotoComparar()
+        {
+            UsuarioRepositorio accesoDatos = new UsuarioRepositorio();
+            Resultado<string> resultado = await accesoDatos.SubirFotoComparar(vista.NombreUsuario, vista.StoreFile, "CM", vista.MoveImage);
+            if (resultado.mensajeError == string.Empty)
+            {
+                string res = resultado.valor;
+                if (res != null)
+                {
+                    ChecarEstatus(res);
+                }
+                else
+                    vista.MostrarMensaje("El usuario no existe, favor de verificar.");
+            }
+            else
+                vista.MostrarMensaje(resultado.mensajeError);
         }
 
         public async void InsertarSesionTrabajo()
         {
             UsuarioRepositorio accesoDatos = new UsuarioRepositorio();
-            Resultado<string> resultado = await accesoDatos.InsertarSesionTrabajo(vista.Ip, vista.Idhardware,vista.NombreUsuario);
+            Resultado<string> resultado = await accesoDatos.InsertarSesionTrabajo(vista.Ip, vista.Idhardware, vista.NombreUsuario);
             if (resultado.mensajeError == string.Empty)
             {
                 string resultadoa = resultado.valor;
@@ -55,6 +88,69 @@ namespace AgenteApp.Presenters
                 vista.MostrarMensaje(resultado.mensajeError);
 
 
+        }
+
+        public async void ChecarEstatus(string idTabla)
+        {
+            UsuarioRepositorio accesoDatos = new UsuarioRepositorio();
+            Resultado<string> resultado = await accesoDatos.ChecarEstatus(vista.NombreUsuario, idTabla);
+            if (resultado.mensajeError == string.Empty)
+            {
+                string res = resultado.valor;
+                if (res != null)
+                {
+                    switch (res)
+                    {
+                        case "TR":
+                            //unica opcion que tiene acceso concedido
+                            //aviso();
+                            //vista.MostrarMensaje("Acceso concedido.");
+                            vista.MostrarMenu(vista.usuario);
+                            ElimitarFaceTemp(idTabla);
+                            break;
+                        //case "SI":
+                        //    //Thread.sleep(3000);
+                        //    aviso2();
+                        //    break;
+                        case "NO":
+                            //Error Intrusion de seguridad compra no autorizada
+                            //aviso3();
+                            vista.MostrarMensaje("Intrusion de seguridad.");
+                            ElimitarFaceTemp(idTabla);
+                            break;
+                        case "AL":
+                            ChecarEstatus(idTabla);
+                            break;
+                        case "CM":
+                            ChecarEstatus(idTabla);
+                            break;
+                        case "NON":
+                            //Error: Template no obtenido, le falta nitidez a la foto. Favor de tomar la foto nuevamente.
+                            //aviso4();
+                            vista.MostrarMensaje("Falta nitidez a la foto, favor de intentar nuevamente");
+                            ElimitarFaceTemp(idTabla);
+                            break;
+                        case "NOI":
+                            //Error: Rostro no identificado. Favor de intentar nuevamente.
+                            //aviso7();
+                            vista.MostrarMensaje("Rostro no identificado. Favor de intentar nuevamente.");
+                            ElimitarFaceTemp(idTabla);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                    vista.MostrarMensaje("El usuario no existe, favor de verificar.");
+            }
+            else
+                vista.MostrarMensaje(resultado.mensajeError);
+        }
+
+        private void ElimitarFaceTemp(string idTabla)
+        {
+            UsuarioRepositorio accesoDatos = new UsuarioRepositorio();
+            accesoDatos.ElimitarFaceTemp(vista.NombreUsuario, idTabla);
         }
     }
 }
